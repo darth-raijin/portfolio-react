52835f9fb2d64f46d0d838b2af485d2591caa475
Mohamed Macow <momac18@student.sdu.dk>
2021-01-02 00:08:42 +0100

    Navbar done

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,68 +1,88 @@
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { Link, BrowserRouter, Route } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFireExtinguisher, faHome, faAddressCard, faTasks, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
+import About from "../../About.js";
+import Projects from "../../Projects.js";
+import Contact from "../../Contact.js";
 
-const Navbar = () => {
+import "../../sass/mystyles.scss";
+
+export default function Navbar() {
+  const [isActive, setisActive] = React.useState(false);
+
   return (
     <div>
-      <nav
-            className="navbar"
-            role="navigation"
-            aria-label="main navigation"
-          >
-            <div className="navbar-brand">
-              <a href="/home" className="navbar-item">
-                <FontAwesomeIcon icon={ faLaptopCode } width="112" height="28" alt="portfolio-logo" color="white" />
-              </a>
+      <BrowserRouter>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a href="/home" className="navbar-item">
+              <FontAwesomeIcon
+                icon={faFireExtinguisher}
+                size = "1em"
+                alt="portfolio-logo"
+                id = "logo-icon"
+              />
+            </a>
 
-              <a
-                role="button"
-                className={"navbar-burger burger"}
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbar"
-              >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
+            <a
+              onClick={() => setisActive(!isActive)}
+              role="button"
+              className={`navbar-burger burger ${
+                isActive ? "active" : "is-active"
+              }`}
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasic"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div id="navbarBasic" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+            <div className="navbar-end">
+              <Link to="/" className="navbar-item">
+              <FontAwesomeIcon
+                icon={faHome}
+                alt="home-icon"
+                id = "icon" 
+              />
+                <h1 id = "icon-text">Home</h1>
+              </Link>
+
+              <Link to="/about" className="navbar-item" id = "link">
+              <FontAwesomeIcon
+                icon={faAddressCard}
+                alt="about-icon"
+                id = "icon"
+              />
+                <h1 id = "icon-text">About</h1>
+              </Link>
+              <Link to="/projects" className="navbar-item" id = "link">
+              <FontAwesomeIcon
+                icon={faTasks}
+                alt="projects-icon"
+                id = "icon"
+              />
+               <h1 id = "icon-text">Projects</h1>
+              </Link>
+              <Link to="/contact" className="navbar-item" id = "link">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                alt="contact-icon"
+                id ="icon"
+              />
+                <h1 id = "icon-text">Contact</h1>
+              </Link>
             </div>
-            <div id="navbar" className="navbar-menu">
-              <div className="navbar-end">
-                <a href="/home" className="navbar-item">
-                  Home
-                </a>
-
-                <a href="/about" className="navbar-item">
-                  About
-                </a>
-                <a href="/projects" className="navbar-item">
-                  Projects
-                </a>
-                <a href="/contact" className="navbar-item">
-                  Contact
-                </a>
-                <div class="navbar-item has-dropdown is-hoverable">
-                  <a class="navbar-a">More</a>
-
-                  <div class="navbar-dropdown">
-                    <a class="navbar-item" href="/about">
-                      About
-                    </a>
-                    <a class="navbar-item" href="/projects">
-                      Projects
-                    </a>
-                    <a class="navbar-item" href="/contact">
-                      Contact
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </nav>
+          </div>
+        </nav>
+        <Route path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </BrowserRouter>
     </div>
   );
-};
-
-export default Navbar;
+}
